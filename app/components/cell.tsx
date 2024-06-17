0f06036ac5b05b4ec5c90b854b79482d4758c08d
{doshishaus} <cgeh0721@mail3.doshisha.ac.jp>
2024-06-17 19:12:59 +0900

    6月17日回転不良、移動不良

--- app/components/cell.tsx
+++ app/components/cell.tsx
@@ -8,6 +8,7 @@ type CellProps = {
 const Cell: React.FC<CellProps> = ({value}) => {
     return(
         <div className={`w-10 h-10 border-2 ${value ? "bg-blue-500":"bg-gray-700"}`}>
+            <p>{value}</p>
         </div>
     );
 };
